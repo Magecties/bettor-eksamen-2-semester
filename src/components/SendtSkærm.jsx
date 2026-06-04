59580a7453ab63c69744c054e7f0affe08c8a03d
Magecties <magnusbbb@hotmail.com>
2026-06-04 22:13:48 +0200

    aftale låst lottie

--- src/components/SendtSkærm.jsx
+++ src/components/SendtSkærm.jsx
@@ -1,5 +1,6 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import emojiExplosion from "../assets/lotties/emoji-explosion.json?url";
+import aftaleLaast from "../assets/lotties/blobbo-aftale-laast.json?url";
 
 export default function SendtSkærm({ modstander, navigate }) {
   const navn = modstander?.name ?? modstander?.username ?? "modstanderen";
@@ -12,6 +13,12 @@ export default function SendtSkærm({ modstander, navigate }) {
         className="sendt-blast"
       />
       <div className="sendt-indhold">
+        <DotLottieReact
+          src={aftaleLaast}
+          loop
+          autoplay
+          className="sendt-blob"
+        />
         <h1>Sendt til<br />{navn}.</h1>
         <p>
           {navn} skal godkende med et tap. Før bettet bliver aktivt. Du får besked så snart det sker.
